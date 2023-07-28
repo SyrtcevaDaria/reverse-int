7bfe52ccd42e3d4ede2d13a9be524e7a734d61fb
SyrtcevaDaria <syrtceva566333@gmail.com>
2023-07-28 20:27:37 +0300

    feat: solve second task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let res = n.toString().split('').reverse().join('');
+    console.log(res);
+    if (res[res.length - 1] === '-') {
+        res = res.slice(0, res.length - 1);
+    }
+    console.log(res);
+    return Number(res)
 }
+module.exports(152);
